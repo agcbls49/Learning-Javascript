51e7121a85e27503177851d101824ab892f3cb8d
Amazing Cabiles <amazinggracecabiles@gmail.com>
2025-08-06 22:22:46 +0800

    8/6/2025

--- classlist.js
+++ classlist.js
@@ -63,3 +63,34 @@ myH1.addEventListener("click", event => {
         event.target.classList.replace("enabled", "disabled");
     }
 });
+
+// class list with node list
+let buttons = document.querySelectorAll('.myButtons');
+
+buttons.forEach(button => {
+    button.classList.add("enabled");
+});
+
+buttons.forEach(button => {
+    button.addEventListener('mouseover', event => {
+        event.target.classList.toggle("hover");
+    })
+});
+
+buttons.forEach(button => {
+    button.addEventListener('mouseout', event => {
+        event.target.classList.toggle("hover");
+    })
+});
+
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+        if (event.target.classList.contains("disabled")) {
+            event.target.textContent += "😲";
+            // see the node list console log
+            console.log(buttons);
+        } else {
+            event.target.classList.replace("enabled", "disabled");
+        }
+    });
+});
